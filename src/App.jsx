0348e07928adf3f7909c0748e2247ec8e3c1566a
Gabriel Prates Bitencourt <07001230162@ifms.edu.br>
2024-03-06 22:17:59 -0400

    inicial template

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+function App() {
+
+  return (
+    <>
+    <div className="flex h-screen w-full justify-center items-center">
+      <Button>Click me</Button>
+    </div>
+    </>
+  )
+}
+
+export default App
